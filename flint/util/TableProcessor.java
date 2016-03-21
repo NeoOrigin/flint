5d22e90d72c6f1bab2c0fc7e90deb083643dd301
Philip Bowditch <pbowditch@users.noreply.github.com>
2016-03-21 11:27:47 +0000

    Update TableProcessor Functions For Fixture Parsing

--- flint/util/TableProcessor.java
+++ flint/util/TableProcessor.java
@@ -3,10 +3,13 @@ package flint.util;
 import java.util.ArrayList;
 
 import fit.Parse;
+
 import flint.data.DataCell;
 import flint.data.DataColumn;
 import flint.data.DataRow;
 import flint.data.DataTable;
+
+import flinet.util.NativeTypeConverter;
 
 /**
  * 
@@ -24,13 +27,25 @@ public class TableProcessor {
      */
     protected bool m_header;
     
+    /**
+     * Is the first line a fixture declaration
+     */
+    protected bool m_fixture;
+    
     
     //--------------------------------------------------------------------------
     
+    /**
+     * Constructor for the TableProcessor class
+     */
     public TableProcessor() {
         this(null);
     }
     
+    /**
+     * Constructor for the TableProcessor class
+     * @param table The fitnesse Parse object representing the entire table
+     */
     public TableProcessor( Parse table ) {
         m_table  = table;
         m_header = true;
@@ -39,12 +54,91 @@ public class TableProcessor {
     
     //--------------------------------------------------------------------------
     
+    /**
+     * Parses an internal fitnesse object converting into a Column for easier processing.
+     * @param cell The cell to parse / interpret
+     */
+    protected DataColumn processColumn( Parse cell ) {
+        StringBuilder b = new StringBuilder( cell.text() );
+        
+        DataColumn dc = DataColumn( "" );
+        
+        // Check if user has asked for results to be returned
+        if ( b.toString().endsWith( "?" ) ) {
+            b = b.deleteCharAt( b.length() - 1 );
+            dc.setAccessModifier( DataColumn.AccessModifier.QUERY );
+        }
+        
+        // Check if user has asked for results to be returned (not equal)
+        if ( b.toString().endsWith( "!" ) ) {
+            b = b.deleteCharAt( b.length() - 1 );
+            dc.setAccessModifier( DataColumn.AccessModifier.QUERY_NO_MATCH );
+        }
+        
+        // Check if the user wants this column ignored and just added it for completeness
+        if ( b.toString().startsWith( "#" ) ) {
+            b = b.deleteCharAt( 0 );
+            dc.setAccessModifier( DataColumn.AccessModifier.IGNORE );
+        }
+        
+        dc.setName( b.toString() );
+        
+        return dc;
+    }
+    
+    /**
+     * Parses a Parse cell turning it into A DataCell for easier processing
+     * @param cell The cell to parse / interpret
+     */
     protected DataCell processCell( Parse cell ) {
         return processCell( cell, null );
     }
     
+    /**
+     * Parses a Parse cell turning it into A DataCell for easier processing
+     * @param cell The cell to parse / interpret
+     * @param col The DataColumn this cell should interpret as
+     */
     protected DataCell processCell( Parse cell, DataColumn col ) {
         return new DataCell( cell.text(), col );
+    }
+    
+    /**
+     * 
+     * @param row The Parse row to interpret as a row of metadata
+     */
+    protected ArrayList<String> processFixtureRow( Parse row ) {
+
+        ArrayList<String> rw = new ArrayList<String>();
+        
+        String dc = null;
+        Parse cell = row.parts;
+            
+        for (int i = 0; cell != null; i++, cell = cell.more) {
+            dc = cell.text();
+            rw.add( dc );
+        }
+        
+        return rw;
+    }
+    
+    /**
+     * 
+     * @param row The Parse row to interpret as a row of columns
+     */
+    protected DataColumn[] processHeaderRow( Parse row ) {
+
+        ArrayList<DataColumn> rw = new ArrayList<DataColumn>();
+        
+        DataColumn dc = null;
+        Parse cell = row.parts;
+            
+        for (int i = 0; cell != null; i++, cell = cell.more) {
+            dc = processColumn( cell );
+            rw.add( dc );
+        }
+        
+        return rw.toArray(new DataColumn[]{});
     }
     
     protected DataRow processRow( Parse row ) {
@@ -52,57 +146,108 @@ public class TableProcessor {
     }
     
     protected DataRow processRow( Parse row, DataColumn[] cols ) {
-        
-        // Provide this simple scenario so we can re-use this method for non data related row processing
-        if ( cols == null ) {
-            ArrayList<DataCell> rw = new ArrayList<DataCell>();
-        
-            DataCell dc = null;
-            Parse cell = row.parts;
-            
-            for (int i = 0; cell != null; i++, cell = cell.more) {
-                dc = processCell( cell );
-                rw.add( dc );
-            }
-        
-            return rw;
-        }
-        
-        
+
         // Mainly for data processing the number of cells must match the number of columns
         int numCells = row.size();
         if ( cols.length != numCells) {
             throw new Exception( "Incorrect number of columns" );
         }
         
-        ArrayList<String> rw = new ArrayList<String>();
+        ArrayList<DataCell> rw = new ArrayList<DataCell>();
         
-        String cellText = null;
+        DataCell dc = null;
+        
+        // Moe to first cell of row
         Parse cell = row.parts;
+        
+        // Iterate over all the cells in the row
         for (int i = 0; cell != null; i++, cell = cell.more) {
-            cellText = processCell( cell, cols[i] );
-            rw.add( cellText );
+            dc = processCell( cell, cols[i] );
+            rw.add( dc );
         }
         
-        return rw;
+        // Concert the array to a DataRow object
+        return new DataRow( rw.toArray( new DataCell[]{} );
+    }
+    
+    /**
+     * Process all subsequent rows as data rows.  Column are optionally associated
+     * @param firstRow The first row in the table to process
+     */
+    protected DataRow[] processDataRows( Parse firstRow ) {
+        return processDataRows( firstRow, null );
+    }
+    
+    /**
+     * Process all subsequent rows as data rows.  Column are optionally associated
+     * @param firstRow The first row in the table to process
+     * @param cols The columns to associate cells with
+     */
+    protected DataRow[] processDataRows( Parse firstRow, DataColumn[] cols ) {
+        
+        Parse currentRow = firstRow;
+        
+        ArrayList<DataRow> rws = new ArrayList<DataRow>();
+        DataRow row = null;
+        
+        // Go through the table converting row by row
+        for (int i = 0; currentRow != null; i++, currentRow = currentRow.more) {
+            row = processRow( currentRow, cols );
+            rws.add( row );
+        }
+        
+        // Export as an array
+        return rws.toArray( new DataRow[]{} );
     }
     
     protected DataTable processTable( Parse table ) {
         DataTable tab = new DataTable();
         
-        Parse row         = table.parts;
+        // Move to first line
+        Parse row = table.parts;
+        if ( row == null ) {
+            return tab;
+        }
+        
+        // Line 1 - Process high level info e.g. fixture type, name, parameters etc
+        if ( m_fixture ) {
+            ArrayList<String> args = processFixtureRow( row );
+            
+            // First 2 cells should be the Fixture name followed by the TypeInstance e.g. CREATE: Customer
+            String fixture = args.remove( 0 );
+            String name    = args.remove( 0 );
+            
+            // Following cells are arguments e.g. OVERWRITE: true, PERMISSIONS: 777
+            Map<String, String> params = NativeTypeConverter.listToMap( args );
+            
+            tab.setFixture( fixture );
+            tab.setName( name );
+            tab.setParameters( params );
+            
+            // Move to next line
+            row = row.more;
+            if ( row == null ) {
+                return tab;
+            }
+        }
+        
+        // Line 2 - Any table columns
         DataColumn[] cols = null;
         
         if ( m_header ) {
-            DataRow headers = processRow( row );
-            row = row.more;
+            cols = processHeaderRow( row );
+            tab.setColumns( cols );
             
-            DataCell[] cells = headers.getCells();
+            // Move to next line
+            row = row.more;
+            if ( row == null ) {
+                return tab;
+            }
         }
         
-        for (int i = 0; row != null; i++, row = row.more) {
-            processRow( row, cols );
-        }
+        // Line 3+ - Process all the data rows
+        DataRow[] rows = processDataRows( row, cols );
+        tab.setRows( rows );
         
         return tab;
     }
@@ -120,6 +265,14 @@ public class TableProcessor {
     
     public Parse getTable() {
         return m_table;
+    }
+    
+    public void setFixturePresent( bool present ) {
+        m_fixture = present;
+    }
+    
+    public bool isFixturePresent() {
+        return m_fixture;
     }
     
     public void setHeaderPresent( bool present ) {
