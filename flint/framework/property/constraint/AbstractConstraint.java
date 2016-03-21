76812d7f00aed8483389f4b6228af26ce2935d1a
Philip Bowditch <pbowditch@users.noreply.github.com>
2016-03-21 14:16:11 +0000

    Update AbstractConstraint.java

--- flint/framework/property/constraint/AbstractConstraint.java
+++ flint/framework/property/constraint/AbstractConstraint.java
@@ -1,7 +1,13 @@
 package flint.framework.property.constraint;
 
+import flint.framework.type.TypeInstance;
+
+/**
+ * Abstract base class used to construct constraints that must pass when setting type properties
+ * @author Philip Bowditch
+ */
 public abstract class AbstractConstraint {
 
-    public bool isValid( TypeInstance inst );
+    public boolean isValid( TypeInstance inst );
     
 }
