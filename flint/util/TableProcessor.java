8fbdca170d19faab48fcc12b486dd181b7310f3a
Philip Bowditch <pbowditch@users.noreply.github.com>
2016-03-21 09:20:48 +0000

    Added Getter/Setter Methods

--- flint/util/TableProcessor.java
+++ flint/util/TableProcessor.java
@@ -2,11 +2,24 @@ package flint.util;
 
 public class TableProcessor {
 
+    /**
+     * Holds the table we wish to process
+     */
     protected Parse m_table;
+    
+    
+    //--------------------------------------------------------------------------
+    
+    public TableProcessor() {
+        this(null);
+    }
     
     public TableProcessor( Parse table ) {
         m_table = table;
     }
+    
+    
+    //--------------------------------------------------------------------------
     
     protected Parse processCell( Parse cell ) {
         return cell;
@@ -20,10 +33,21 @@ public class TableProcessor {
         return table;
     }
     
+    
+    //--------------------------------------------------------------------------
+    
     public ArrayList<ArrayList<String>> process() {
         ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();
         
         return arr;
+    }
+    
+    public void setTable( Parse table ) {
+        m_table = table;
+    }
+    
+    public Parse getTable() {
+        return m_table;
     }
     
 }
