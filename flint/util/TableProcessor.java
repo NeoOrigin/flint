a29cf651de3d10cf4b769aa230393e0d08df22c6
Philip Bowditch <pbowditch@users.noreply.github.com>
2016-03-21 10:25:11 +0000

    Update TableProcessor.java

--- flint/util/TableProcessor.java
+++ flint/util/TableProcessor.java
@@ -1,11 +1,28 @@
 package flint.util;
 
+import java.util.ArrayList;
+
+import fit.Parse;
+import flint.data.DataCell;
+import flint.data.DataColumn;
+import flint.data.DataRow;
+import flint.data.DataTable;
+
+/**
+ * 
+ * @author Philip Bowditch
+ */
 public class TableProcessor {
 
     /**
      * Holds the table we wish to process
      */
     protected Parse m_table;
+    
+    /**
+     * Is a header present in the table
+     */
+    protected bool m_header;
     
     
     //--------------------------------------------------------------------------
@@ -15,31 +32,86 @@ public class TableProcessor {
     }
     
     public TableProcessor( Parse table ) {
-        m_table = table;
+        m_table  = table;
+        m_header = true;
     }
     
     
     //--------------------------------------------------------------------------
     
-    protected Parse processCell( Parse cell ) {
-        return cell;
+    protected DataCell processCell( Parse cell ) {
+        return processCell( cell, null );
     }
     
-    protected Parse processRow( Parse row ) {
-        return row;
+    protected DataCell processCell( Parse cell, DataColumn col ) {
+        return new DataCell( cell.text(), col );
     }
     
-    protected Parse processTable( Parse table ) {
-        return table;
+    protected DataRow processRow( Parse row ) {
+        return  processRow( row, null );
     }
     
-    
-    //--------------------------------------------------------------------------
-    
-    public ArrayList<ArrayList<String>> process() {
-        ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();
+    protected DataRow processRow( Parse row, DataColumn[] cols ) {
         
-        return arr;
+        // Provide this simple scenario so we can re-use this method for non data related row processing
+        if ( cols == null ) {
+            ArrayList<DataCell> rw = new ArrayList<DataCell>();
+        
+            DataCell dc = null;
+            Parse cell = row.parts;
+            
+            for (int i = 0; cell != null; i++, cell = cell.more) {
+                dc = processCell( cell );
+                rw.add( dc );
+            }
+        
+            return rw;
+        }
+        
+        
+        // Mainly for data processing the number of cells must match the number of columns
+        int numCells = row.size();
+        if ( cols.length != numCells) {
+            throw new Exception( "Incorrect number of columns" );
+        }
+        
+        ArrayList<String> rw = new ArrayList<String>();
+        
+        String cellText = null;
+        Parse cell = row.parts;
+        for (int i = 0; cell != null; i++, cell = cell.more) {
+            cellText = processCell( cell, cols[i] );
+            rw.add( cellText );
+        }
+        
+        return rw;
+    }
+    
+    protected DataTable processTable( Parse table ) {
+        DataTable tab = new DataTable();
+        
+        Parse row         = table.parts;
+        DataColumn[] cols = null;
+        
+        if ( m_header ) {
+            DataRow headers = processRow( row );
+            row = row.more;
+            
+            DataCell[] cells = headers.getCells();
+        }
+        
+        for (int i = 0; row != null; i++, row = row.more) {
+            processRow( row, cols );
+        }
+        
+        return tab;
+    }
+    
+    
+    //--------------------------------------------------------------------------
+    
+    public DataTable process() {
+        return processTable( m_table );
     }
     
     public void setTable( Parse table ) {
@@ -48,6 +120,14 @@ public class TableProcessor {
     
     public Parse getTable() {
         return m_table;
+    }
+    
+    public void setHeaderPresent( bool present ) {
+        m_header = present;
+    }
+    
+    public bool isHeaderPresent() {
+        return m_header;
     }
     
 }
