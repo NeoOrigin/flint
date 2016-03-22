9f9924b9768b63f7aa8ef09ff05080997b257310
Philip Bowditch <pbowditch@users.noreply.github.com>
2016-03-22 09:48:59 +0000

    Updated to handle different ways of defining

--- flint/fixtures/DefineFixture.java
+++ flint/fixtures/DefineFixture.java
@@ -1,6 +1,8 @@
 package flint.fixture;
 
 // Core Java classes
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Properties;
 
 // 3rd party classes
@@ -11,7 +13,12 @@ import flint.data.DataColumn;
 import flint.data.DataRow;
 import flint.data.DataTable;
 import flint.environment.Environment;
+import flint.framework.BaseFramework;
+import flint.framework.property.AttributeProperty;
+import flint.framework.type.TypeDefinition;
 import flint.util.NameNormalizer;
+import flint.util.NativeTypeConverter;
+import flint.util.TableProcessor;
 
 /**
  * A simple fixture used to define type declarations that can later be instantiated / declared
@@ -42,6 +49,8 @@ public class DefineFixture extends Fixture {
      */
     public DefineFixture( Environment env, String label ) {
         m_environment = environment;
+        
+        // We re-read the label so jus ignore it, might eventualyl remove it
     }
     
     
@@ -52,7 +61,7 @@ public class DefineFixture extends Fixture {
      * @param table The Parse representing the table being parsed
      */
     @Override
-    public void doTable(Parse table) {
+    public void doTable( Parse table ) {
         super.doTable(table);
         
         // Create a table to encapsulate all the data
@@ -60,21 +69,28 @@ public class DefineFixture extends Fixture {
         processor.setTable( table );
         //processor.setHeader( false );
         
-        DataTable dt = processor.process();
+        DataTable      dt        = processor.process();
         BaseFramework  framework = m_environment.getFramework();
-        Map props = new Properties();
+        Map            props     = new Properties();
         
-        props = keyValueStrategy( props, dt );
-        //props = tableStrategy( props, dt );
+        // For now hard coded but allows for "some" extension going forwards
+        switch ( "keyValueStrategy" ) {
+            
+            case "keyValueStrategy" ) props = keyValueStrategy( props, dt );
+                                      break;
+            case "tableStrategy"    ) props = tableStrategy( props, dt );
+                                      break;
+                                   
+        }
         
         // Extract core fields from the type definition
-        TypeDefinition t         = new TypeDefinition();
-        String         typeName  = dt.getName();
-        Iterator it = null;
-        String stringStr = NameNormalizer.normalizeName( "name"  );
-        String aliasStr  = NameNormalizer.normalizeName( "alias" );
-        String[] names = null;
-        String name = null;
+        TypeDefinition    t         = new TypeDefinition();
+        String            stringStr = NameNormalizer.normalizeName( "name"  );
+        String            aliasStr  = NameNormalizer.normalizeName( "alias" );
+        String[]          names     = null;
+        String            name      = null;
+        Iterator          it        = null;
+        AttributeProperty ap        = null;
         
         // Finished reading all the properties, re-add them back to the definition
         t.setProperties( props );
@@ -85,39 +101,46 @@ public class DefineFixture extends Fixture {
         it = props.keySet().iterator();
             
         while ( it.hasNext() ) {
-            name  = (String)it.next();
-            AttributeProperty  pValue = (AttributeProperty)props.get( name );
+            name = (String)it.next();
+            ap   = (AttributeProperty)props.get( name );
                 
-            // Name and alias are considered core properties
+            // A definition may have many names e.g. CSV, COMMA SEPERATED VALUES, DELIMITED etc
+            // Register it under all these names
             if ( name.equalsIgnoreCase( stringStr ) || name.equalsIgnoreCase( aliasStr ) ) {
                 
-                names = NativeTypeConverter.getStringAsList( c.getValue() );
+                names = NativeTypeConverter.getStringAsList( ap.getValue() );
                     
                 registerDefinition( framework, t, names );
             }
         }
         
-        registerDefinition( framework, t, new String[]{ typeName } );
+        // Register the definition as the label in the data table e.g. DEFINE: CSV
+        registerDefinition( framework, t, new String[]{ dt.getName() } );
     }
     
     protected void registerDefinition( framework, TypeDefinition t, String[] names ) {
-        if ( names != null ) {
-            Map<String, TypeDefinition> registeredTypes = framework.getTypes();
+        // Do nothing if we have no names
+        if ( names == null ) {
+            return;
+        }
+        
+        Map<String, TypeDefinition> registeredTypes = framework.getTypeDefinitions();
             
-            for ( String alias : names ) {
-                registeredTypes.put( alias, t );
-            }
+        for ( String alias : names ) {
+            registeredTypes.put( alias, t );
         }
     }
     
     protected Property addProperty( Properties props, String name, String value, String subcategory ) {
 
+        // Firstly ge the property if it already exists else create it e.g. "PATH" or "PERMISSIONS"
         AttributeProperty ap = (AttributeProperty)props.get( name );
         if ( ap == null ) {
             ap = new AttributeProperty( name );
+            ap.getAttributes().put( "NAME", name );
         }
         
-        // Check if a subcategory was specified e.g. COMPRESS.READ_ONLY
+        // Check if a subcategory was specified e.g. PATH.READ_ONLY
         // Just incase the user changes how things normalize we ensure this subcategory is formatted
         // correctly
         
@@ -134,69 +157,16 @@ public class DefineFixture extends Fixture {
         return props;
     }
     
-    /**
-     * Splits a key into a category, name, and subcategory
-     * @param key The key to split
-     * @return 
-     */
-    protected static String[] splitKey( final String key ) {
-        StringBuilder b = new StringBuilder( "\\" );
-        b = b.append( name_divider );
-        
-        String keyName = NameNormalizer.normalizeName(key);
-        return keyName.split( b.toString() );
-    }
-    
-    /**
-     * All properties have a name, this extracts the name from the property
-     * @param key The full property specifier
-     * @return The name of the property else null if not found
-     */
-    public static String extractName( final String key ) {
-        String name = "";
-        String[] names = splitKey( key );
-            
-        // The full property specification is something like this
-        // name([.subcategory]*)
-        // E.g. COMPRESS.READ_ONLY, COMPRESS.DEFAULT, COMPRESS.VALUES
-        if ( names.length >= 1 ) {
-            name = names[0].trim();
-        }
-
-        return name;
-    }
-    
-    /**
-     * Most properties have a subcategory, the subcategory typically identifies
-     * a configuration option for the property or identifies various abilities
-     * options that it can contain.
-     * @param key The full property specifier
-     * @return The subcategory of the property else null if not found
-     */
-    public static String extractSubCategory( final String key ) {
-        String name    = "";
-        String[] names = splitKey( key );
-
-        // Return the sub category if any is given, it normally should...
-        // name([.subcategory]*)
-        // E.g. COMPRESS.READ_ONLY, COMPRESS.DEFAULT, COMPRESS.VALUES
-        if ( names.length > 1 ) {
-            name = names[1].trim();
-        }
-
-        return name;
-    }
-    
     public Map keyValueStrategy( Map props, DataTable dt ) {
         
-        String    key      = null;
-        String    value    = null;
-        DataRow[] rows     = dt.getRows();    // Rows in the table
-        String[]  cells    = null;            // Holds cells within current Row
-        String name = null;
-        String subcategory = null;
-        
-        Map newProps = props;
+        String    key         = null;
+        String    value       = null;
+        DataRow[] rows        = dt.getRows();    // Rows in the table
+        String[]  cells       = null;            // Holds cells within current Row
+        String    name        = null;
+        String    subcategory = null;
+        Map       newProps    = props;
+        String[]  names       = null;
         
         // Go through all rows adding as we go
         for ( int i = 0; i < rows.length; i++ ) {
@@ -210,12 +180,27 @@ public class DefineFixture extends Fixture {
             
             // Go through all the cells in the row, should be pairs
             for ( int j = 0; j < cells.length; j += 2 ) {
-                key   = cells[ j     ];
-                value = cells[ j + 1 ];
+                key         = cells[ j     ];
+                value       = cells[ j + 1 ];
                 
-                name = extractName( key );
-                subcategory = extractSubCategory( key );
+                // Normalize the key
+                key         = NameNormalizer.normalizeName(key);
+                names       = key.split( name_divider );
+
+                // We should have got something, error if we didnt
+                if ( names.length <= 0 ) {
+                    throw new Exception( "Unknown property being defined, name has not been specified" );
+                }
                 
+                name        = names[0].trim();
+                subcategory = "";
+                
+                // Subcategory is optional extract it if its there
+                if ( names.length > 1 ) {
+                    subcategory = names[1].trim();
+                }
+                
+                // Add this property
                 newProps = addProperty( newProps, name, value, subcategory );
             }
         }
@@ -224,16 +209,19 @@ public class DefineFixture extends Fixture {
     }
     
     public Map tableStrategy( Map props, DataTable dt ) {
-        DataColumn[]   cols      = dt.getColoumns();
-        DataRow[]      rows      = dt.getRows();
-        String[]       cells     = null;
-        Map            newProps  = props;
-        String name = null;
-        String subcategory = null;
-        int nameIndex = -1;
+        DataColumn[]   cols        = dt.getColoumns(); // Columns in the data table
+        DataRow[]      rows        = dt.getRows();     // The data / properties
+        String[]       cells       = null;
+        Map            newProps    = props;
+        String         name        = null;
+        String         subcategory = null;
+        int            nameIndex   = -1;
         
+        // Find where the property name can be found
         for ( int j = 0; j < cols.length; j++ ) {
+            
             name = cols[j].getName();
+            
             if ( name.equalsIgnoreCase( "name" ) ) {
                 nameIndex = j;
                 break;
@@ -241,20 +229,26 @@ public class DefineFixture extends Fixture {
             
         }
         
+        // Error if we didnt find it
         if ( nameIndex < 0 ) {
-            throw new Exception();
+            throw new Exception( "Name column not found on Definition" );
         }
         
+        // Now go through all the data
         for ( int i = 0; i < rows.length; i++ ) {
+            
+            // Get the data in this row and compare length is the same as the columns
             cells = rows[i].getCells();
             if ( cells.length != cols.length ) {
-                throw new Exception();
+                throw new Exception( "Length of this row does not match the number of columns defined" );
             }
             
             // Add each column as a key value pair
             for ( int j = 0; j < cols.length; j++ ) {
-                name = cells[nameIndex];
+                name        = cells[nameIndex];
                 subcategory = cols[j].getName();
+                
+                // Add this property
                 newProps = addProperty( newProps, name, cells[j], subcategory );
             }
         }
