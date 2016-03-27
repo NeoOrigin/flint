6f4b43766c1c56f2e1bc43b3301248e916379742
Philip Bowditch <pbowditch@users.noreply.github.com>
2016-03-27 21:32:37 +0100

    Added NoOp for usability

---
+++ flint/data/aggregate/NoOpAggregator.java
@@ -0,0 +1,37 @@
+package flint.data.aggregate;
+
+/**
+ *
+ * @author Philip Bowditch
+ */
+public class NoOpAggregator extends AbstractAggregator {
+
+    protected String m_amount;
+    
+    public NoOpAggregator() {
+        super();
+        
+        m_name       = "Value";
+        m_amount     = "";
+    }
+    
+    @Override
+    public void reset() {
+        m_amount = "";
+    }
+    
+    @Override
+    public void aggregate( String value ) {
+    }
+    
+    @Override
+    public String getResult() {
+        return m_amount;
+    }
+    
+    @Override
+    public void setResult( String result ) {
+        m_amount = result;
+    }
+    
+}
