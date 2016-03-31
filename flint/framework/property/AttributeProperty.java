59961f7b6bb3e340525485a5615da80bee2b9d5e
Philip Bowditch <pbowditch@users.noreply.github.com>
2016-03-31 08:16:29 +0100

    Added more methods

--- flint/framework/property/AttributeProperty.java
+++ flint/framework/property/AttributeProperty.java
@@ -1,10 +1,24 @@
 package flint.framework.property;
 
 /**
- *
+ * An implementation of a property thats sets
+ * core attributes such as if its read only, has a default etc
  * @author Philip Bowditch
  */
 public class AttributeProperty extends AbstractProperty {
+    
+    /**
+     * Determines who can see this property
+     *
+     * ALL    - Everything, no restriction
+     * ENGINE - Not visible in the ui
+     * FRAMEWORK - Not visible in the ui or passed to the engine
+     */
+    public enum VisibilityLevel {
+        ALL,
+        ENGINE,
+        FRAMEWORK
+    }
     
     /**
      * Determines whether the property has any effect
@@ -19,7 +33,7 @@ public class AttributeProperty extends AbstractProperty {
     /**
      * Is this property hidden from the user
      */
-    protected boolean m_hidden;
+    protected VisibilityLevel m_hidden;
     
     /**
      * Textual description for ease of use
@@ -56,7 +70,7 @@ public class AttributeProperty extends AbstractProperty {
         
         m_disabled      = false;
         m_readOnly      = false;
-        m_hidden        = false;
+        m_hidden        = VisibilityLevel.ALL;
         m_description   = "";
         m_optional      = false;
         m_default       = null;
@@ -68,6 +82,23 @@ public class AttributeProperty extends AbstractProperty {
     
     //--------------------------------------------------------------------------
     
+    
+    public boolean isDisabled() {
+        return m_disabled;
+    }
+    
+    public void setDisabled( boolean disabled ) {
+        m_disabled = disabled;
+    }
+    
+    public boolean isReadOnly() {
+        return m_readOnly;
+    }
+    
+    public void setReadOnly
+    ( boolean readOnly ) {
+        m_readOnly = readOnly
+    }
     
     public boolean isOptional() {
         return m_optional;
@@ -91,5 +122,29 @@ public class AttributeProperty extends AbstractProperty {
     
     public void setType( String typ ) {
         m_type = typ;
+    }
+    
+    public VisibilityLevel getVisibilityLevel() {
+        return m_hidden;
+    }
+    
+    public void setVisibilityLevel( VisibilityLevel hidden ) {
+        m_hidden = hidden;
+    }
+    
+    public String getDescription() {
+        return m_description;
+    }
+    
+    public void setDescription( String description ) {
+        m_description = description;
+    }
+    
+    public String[] getAllowedValues() {
+        return m_allowedValues;
+    }
+    
+    public void setAllowedValues( String[] allowed ) {
+        m_allowedValues = allowed;
     }
 }
